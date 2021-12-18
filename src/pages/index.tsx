9580034fed5f2a1813fc3df670feec40f0f23eff
= <hotalog@hota1024.com>
2021-12-18 23:26:07 +0900

    :tada: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - quick-py</title>
+      </Head>
+
+      <div>quick-py</div>
+    </>
+  )
+}
+
+export default HomePage
